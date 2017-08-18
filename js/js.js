4cfcdeea8894ad85b01f9e213be61ab784789539
Steven Mayo <jsmayo91@gmail.com>
2017-08-17 20:06:54 -0400

    Corrects: html for div block creation

--- js/js.js
+++ js/js.js
@@ -1,1 +1,12 @@
-alert("hey");
+var etches = "";
+var mainCont = $(".mainContainer").width();
+var pixels = 70; 
+// this will be from user input, but given a default val later
+
+for(var i =0; i <= mainCont/pixels; i++) {
+		etches += '<div class="etches"></div>';
+	
+}
+
+$(".mainContainer").append(etches);
+// use js to add styling after creation.. or use $("document").ready() approach
