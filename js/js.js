e158c37b47fa53af000fd25c7558ed407baa113d
Steven Mayo <jsmayo91@gmail.com>
2019-04-10 20:07:56 -0400

    Note added.

--- js/js.js
+++ js/js.js
@@ -3,10 +3,11 @@ var mainCont = $(".mainContainer").width();
 var pixels = 70; 
 // this will be from user input, but given a default val later
 
-for(var i =0; i <= mainCont/pixels; i++) {
-		etches += '<div class="etches"></div>';
+for(var i = 0; i <= mainCont/16; i++) {
+		etches += `<div class="etches"></div>`;
 	
 }
 
 $(".mainContainer").append(etches);
 // use js to add styling after creation.. or use $("document").ready() approach
+// legit need to make the action trigger on mouse event..
